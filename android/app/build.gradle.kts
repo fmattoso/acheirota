bc2deba6e3b1233c5b5770f93c03dd99020b8652
fmattoso <fabiano.mattoso@gmail.com>
2026-02-02 16:44:58 -0300

    api key em local.properties

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,6 +5,16 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+// Carregar propriedades do local.properties
+val localProperties = Properties()
+val localPropertiesFile = rootProject.file("local.properties")
+if (localPropertiesFile.exists()) {
+    localProperties.load(localPropertiesFile.inputStream())
+}
+
+// Obter a chave do Google Maps
+val mapsApiKey = localProperties.getProperty("mapsApiKey") 
+
 android {
     namespace = "com.brdata.acheirota"
     compileSdk = flutter.compileSdkVersion
@@ -28,13 +38,25 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
-    }
+
+        // Passar a chave para o AndroidManifest
+        manifestPlaceholders["mapsApiKey"] = mapsApiKey
+
+        // Também disponível como build config
+        buildConfigField("String", "\"$mapsApiKey\"")
+   }
 
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
+
+            // Para release, você pode querer uma chave diferente
+            // manifestPlaceholders["mapsApiKey"] = "SUA_CHAVE_RELEASE"
+        }
+        debug {
+            manifestPlaceholders["mapsApiKey"] = mapsApiKey
         }
     }
 }
